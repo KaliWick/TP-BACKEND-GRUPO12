2cfa23480ad89f836f5d81998be3f07f39edc883
KaliWick <168383538+KaliWick@users.noreply.github.com>
2024-07-11 23:45:48 -0300

    d

--- public/js/inicioSesion.js
+++ public/js/inicioSesion.js
@@ -4,54 +4,51 @@ document.addEventListener('DOMContentLoaded', () => {
     inicioSesionForm.addEventListener('submit', async (e) => {
         e.preventDefault();
         if (validateForm(e)) {
-            const formData = new FormData(inicioSesionForm);
-            const data = {
-                email: formData.get('email'),
-                password: formData.get('contrasenia')
-            };
+        const formData = new FormData(inicioSesionForm);
+        const data = {
+            email: formData.get('email'),
+            password: formData.get('contrasenia')
+        };
+        alert('Datos del formulario: ' + JSON.stringify(data));
+        try {
+            const response = await fetch('/inicioSesion/inicioSesion', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
 
-            alert('Datos del formulario: ' + JSON.stringify(data)); // Muestra los datos del formulario
-
-            try {
-                const response = await fetch('/inicioSesion/iniciar_sesion', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(data)
-                });
-
-                if (!response.ok) {
-                    throw new Error('Error en la solicitud');
-                }
-
-                const result = await response.json();
-                alert('Resultado del inicio de sesión: ' + JSON.stringify(result)); // Muestra el resultado del inicio de sesión
-
-                if (result.success) {
-                    alert('Inicio de sesión exitoso, redirigiendo...'); // Muestra mensaje de éxito
-                    window.location.href = '/';
-                } else {
-                    alert('Inicio de sesión fallido: ' + result.error); // Muestra mensaje de error
-                }
-            } catch (error) {
-                console.error('Error:', error);
-                alert('Hubo un error al iniciar sesión');
+            if (!response.ok) {
+                throw new Error('Error en la solicitud');
             }
+
+            const result = await response.json();
+            alert('Resultado del inicio de sesión: ' + JSON.stringify(result));
+
+            if (result.success) {
+                alert('Inicio de sesión exitoso, redirigiendo...');
+                window.location.href = '/';
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Hubo un error al iniciar sesión');
         }
+    }
     });
 });
 
 function validateForm(event) {
+    //inputs
     let email = document.getElementById("email").value;
     let contrasenia = document.getElementById("contrasenia").value;
 
     if (email.trim() === "") {
-        alert("Por favor ingrese un correo");
+        alert("Por favor ingrese un mail");
         return false;
     }
     if (contrasenia.trim() === "") {
-        alert("Por favor ingrese una contraseña");
+        alert("Por favor ingrese una contraseña")
         return false;
     }
     return true;
